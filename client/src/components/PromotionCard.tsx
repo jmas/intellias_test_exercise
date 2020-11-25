fbfe496eba05a2d35fd89350e0631a3eeccab483
jmas <jmas.ukraine@gmail.com>
2020-11-25 21:50:05 +0200

    Update promo

--- client/src/components/PromotionCard.tsx
+++ client/src/components/PromotionCard.tsx
@@ -5,6 +5,7 @@ import { Promotion } from '../types';
 export default function PromotionCard({ promotion, onClick }: { promotion: Promotion, onClick?: () => void }) {
   return (
     <Card
+      style={{ height: 257, backgroundColor: '#f0f0f0' }}
       onClick={onClick}
     >
       <Card.Meta title={promotion.text} />
